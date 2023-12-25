413089ecbe1f887854141d405308b6414c3d7e0f
neshamon <jmmatthews@proton.me>
2023-12-24 22:19:57 -0600

    Generate angles of pi

--- ep1.lisp
+++ ep1.lisp
@@ -3,3 +3,13 @@
 (defsketch test ()
   (rect 100 100 200 200))
 (make-instance 'test)
+
+(defsketch trig ()
+  (defun create-angles (a)
+    (if (> a (* pi 2))
+        (* pi 2)
+        (progn
+          (princ (sin a))
+          (create-angles (+ a 0.1))))))
+
+(create-angles 0)
